8e861472b9a64587454cf1509228937960a9c290
superwomanliuda <abbyliuwp@gmail.com>
2024-03-21 19:25:09 +0800

    vercel

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/webhook", webhookRouter);
 
-// 设置其他路由来管理Stream，例如创建Stream的路由
 app.post("/create-stream", async (req, res) => {
   const streamDetails = {
     webhookUrl: "https://music-nft-mu.vercel.app/api/webhook",
@@ -25,15 +24,31 @@ app.post("/create-stream", async (req, res) => {
     tag: "album_created",
     chains: ["0x13881"],
     includeNativeTxs: true,
-    abi: AlbumFactoryABI,
-    allAddresses: true,
-    topic0: ["AlbumCreated(address,address)"],
+    //abi: AlbumFactoryABI,
+    //topic0: ["AlbumCreated(address,address)"],
   };
 
   try {
     const stream = await Moralis.Streams.add(streamDetails);
     res.json(stream.toJSON());
   } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// 注意：这个操作通常在流创建后立即执行，所以最好是在创建流后自动执行或者通过另一个独立的请求来执行
+app.post("/add-address-to-stream", async (req, res) => {
+  const { streamId, address } = req.body; // 假设请求中包含了 streamId 和要添加的 address
+
+  try {
+    const response = await Moralis.Streams.addAddress({
+      id: streamId,
+      address: [address], // 这里可以是一个地址的数组
+    });
+    console.log("Address added to stream:", response.toJSON());
+    res.json(response.toJSON());
+  } catch (error) {
+    console.error("Error adding address to stream:", error);
     res.status(500).json({ error: error.message });
   }
 });
