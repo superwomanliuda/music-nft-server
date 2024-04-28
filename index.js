a482adace39385d81a60c61a3555b76f49eb1e0b
superwomanliuda <abbyliuwp@gmail.com>
2024-04-28 20:47:23 +0800

    Update backend to add IPFS proxy endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 // CORS配置
 const corsOptions = {
-  origin: "http://localhost:3000", // 前端托管之后记得修改此处
+  origin: "https://music-nft-server-b8ff3.web.app", // 前端托管之后记得修改此处
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -47,42 +47,6 @@ app.post("/generateToken", (req, res) => {
       res.status(500).send("Error creating custom token");
     });
 });
-
-/*app.post("/create-stream", async (req, res) => {
-  const streamDetails = {
-    webhookUrl: "https://music-nft-mu.vercel.app/api/webhook",
-    description: "Listen to AlbumCreated events",
-    tag: "album_created",
-    chains: ["0x13881"],
-    includeNativeTxs: true,
-    abi: AlbumFactoryABI,
-    topic0: ["AlbumCreated(address,address)"],
-  };
-
-  try {
-    const stream = await Moralis.Streams.add(streamDetails);
-    res.json(stream.toJSON());
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});*/
-
-// 注意：这个操作通常在流创建后立即执行，所以最好是在创建流后自动执行或者通过另一个独立的请求来执行
-/*app.post("/add-address-to-stream", async (req, res) => {
-  const { streamId, address } = req.body; // 假设请求中包含了 streamId 和要添加的 address
-
-  try {
-    const response = await Moralis.Streams.addAddress({
-      id: streamId,
-      address: [address], // 这里可以是一个地址的数组
-    });
-    console.log("Address added to stream:", response.toJSON());
-    res.json(response.toJSON());
-  } catch (error) {
-    console.error("Error adding address to stream:", error);
-    res.status(500).json({ error: error.message });
-  }
-});*/
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
