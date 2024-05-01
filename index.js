7fef1475572a00a76b20f9660cb07aa13716a4eb
superwomanliuda <abbyliuwp@gmail.com>
2024-05-01 15:52:20 +0800

    locolhost test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 // CORS配置
 const corsOptions = {
-  origin: "https://music-nft-server-b8ff3.web.app", // 前端托管之后记得修改此处
+  origin: "http://localhost:3000/", // 前端托管之后记得修改此处
 };
 app.use(cors(corsOptions));
 app.use(express.json());
